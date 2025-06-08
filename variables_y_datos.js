9f3f89bf1a245210d729a972a09dab6da5c2f0ef
Darmin Reyes <119169294+Darmin-RC@users.noreply.github.com>
2025-06-07 21:37:02 -0400

    Create variables_y_datos.js

---
+++ variables_y_datos.js
@@ -0,0 +1,25 @@
+// comentario de una línea
+
+/*
+Commentario
+De
+bloque
+*/
+
+// Luego de que asignamos un valor a una constante, su valor no cambiará 
+const pi = 3.14 // SIEMPRE SERÁ 3.14
+
+// Con las variables (var) sí puede cambiar su valor durante la ejecución
+var edad = 19 // valor inicial
+edad = 20 // nuevo valor
+
+// tipos de datos
+
+const numero = 10
+const bool = true
+const float = 33.2
+const arreglo = [2, 3, 4, 5,]
+const objeto = {
+  nombre: "Darmin",
+  edad: 19
+}
